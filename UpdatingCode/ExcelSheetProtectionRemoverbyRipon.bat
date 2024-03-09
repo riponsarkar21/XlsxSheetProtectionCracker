68880890f547b9e1930b9f556d8b66c5230d3694
riponsarkar21 <141989340+riponsarkar21@users.noreply.github.com>
2024-03-09 19:31:36 +0600

    Add files via upload

---
+++ UpdatingCode/ExcelSheetProtectionRemoverbyRipon.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem Set the path to your Miniconda installation
+set CONDA_PATH=C:\Users\USER\miniconda3
+
+rem Set the name of your virtual environment
+set ENV_NAME=myenv
+
+rem Set the path to your Jupyter Notebook file
+set NOTEBOOK_PATH=C:\Users\USER\pypy\Excel Sheet Protection Remover by Ripon\ExcelSheetProtectionRemoverbyRipon.bat
+
+
+
+rem Activate the virtual environment
+call "%CONDA_PATH%\Scripts\activate.bat" %ENV_NAME%
+
+rem Run the Jupyter Notebook
+jupyter nbconvert --execute --to notebook --inplace "%NOTEBOOK_PATH%"
+
+rem Deactivate the virtual environment
+call "%CONDA_PATH%\Scripts\deactivate.bat"
+
+rem Show a message box indicating completion
+msg * "Jupyter Notebook execution completed!"
